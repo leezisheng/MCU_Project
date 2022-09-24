1bb74aeae50b9d35504dca3ba1a1163c3880058c
leezisheng <a413liqingshui@gmail.com>
2022-09-24 17:21:11 +0800

    Complete all functions of gyroscope part, not tested yet

--- Hardware/USART_Gyroscope/USART_Gyroscope .c
+++ Hardware/USART_Gyroscope/USART_Gyroscope .c
@@ -23,15 +23,15 @@
 /* ++++++++++++++++++++++++++++++Command string++++++++++++++++++++++++++++++ */
 
 /* The z-axis Angle goes to zero */
-const char YAWCMD[3] = {0XFF,0XAA,0X52};
+const uint8_t YAWCMD[3]     	= {0XFF,0XAA,0X52};
 /* Calibration of acceleration */
-const char ACCCMD[3] = {0XFF,0XAA,0X67};
+const uint8_t ACCCMD[3] 		= {0XFF,0XAA,0X67};
 /* Toggles sleep mode and working mode */
-const char SLEEPCMD[3] = {0XFF,0XAA,0X60};
+const uint8_t SLEEPCMD[3] 		= {0XFF,0XAA,0X60};
 /* Set the serial port output mode of the gyroscope */
-const char UARTMODECMD[3] = {0XFF,0XAA,0X61};
+const uint8_t UARTMODECMD[3] 	= {0XFF,0XAA,0X61};
 /* Set the gyro IIC output mode */
-const char IICMODECMD[3] = {0XFF,0XAA,0X62};
+const uint8_t IICMODECMD[3] 	= {0XFF,0XAA,0X62};
 
 /* Data cache array */
 static unsigned char ucRxBuffer[250];
@@ -136,19 +136,143 @@ t_FuncRet Send_Command(uint8_t data[3])
 }
 
 /** 
-* @description: Data conversion 
+* @description: Gyroscope calibration
+* @param  {void} 
+* @return {{t_FuncRet} : if success , return (t_FuncRet)Operatin_Success
+* @author: leeqingshui 
 */
-void Get_TrueData(void)
+t_FuncRet Gyroscope_Calibration(void)
+{
+	t_FuncRet ret = (t_FuncRet)Operatin_Success;
+	
+	/* Calibration of acceleration */
+	ret = Send_Command(ACCCMD);
+	
+	if(ret == Operatin_Fail)
+	{
+		return (t_FuncRet)ret;
+	}
+	/* Wait until the internal calibration of the module is good. The internal calculation of the module will take some time */
+	HAL_Delay(100);
+	
+	/* Z axis Angle calibration */
+	ret = Send_Command(YAWCMD);
+	if(ret == Operatin_Fail)
+	{
+		return (t_FuncRet)ret;
+	}
+	/* Wait until the internal calibration of the module is good. The internal calculation of the module will take some time */
+	HAL_Delay(10);
+
+	return (t_FuncRet)ret;
+}
+
+
+
+/** 
+* @description: Return the X-axis acceleration
+* @param  {void} 
+* @return {{float} : the X-axis acceleration
+* @author: leeqingshui 
+*/
+float Get_Xaxis_Acc(void)
 {
 	acc_x = (float)S_Acc.Acc_DataBuf[0]/32768*16;
+	return acc_x;
+}
+
+/** 
+* @description: Return the Y-axis acceleration
+* @param  {void} 
+* @return {{float} : the Y-axis acceleration
+* @author: leeqingshui 
+*/
+float Get_Yaxis_Acc(void)
+{
 	acc_y = (float)S_Acc.Acc_DataBuf[1]/32768*16;
+	return acc_y;
+}
+
+/** 
+* @description: Return the Z-axis acceleration
+* @param  {void} 
+* @return {{float} : the Z-axis acceleration
+* @author: leeqingshui 
+*/
+float Get_Zaxis_Acc(void)
+{
 	acc_z = (float)S_Acc.Acc_DataBuf[2]/32768*16;
-		
-	gyro_x = (float)S_AngleAcc.Angle_Acc_DataBuf[0]/32768*2000;
-	gyro_y = (float)S_AngleAcc.Angle_Acc_DataBuf[1]/32768*2000;
-	gyro_z = (float)S_AngleAcc.Angle_Acc_DataBuf[2]/32768*2000;
-		
+	return acc_z;
+}
+
+/** 
+* @description: Return the X-axis angel
+* @param  {void} 
+* @return {{float} : the X-axis angel
+* @author: leeqingshui 
+*/
+float Get_Xaxis_Angle(void)
+{
 	angle_x = (float)S_Angle.Angle_DataBuf[0]/32768*180;
+	return angle_x;
+}
+
+/** 
+* @description: Return the Y-axis angel
+* @param  {void} 
+* @return {{float} : the Y-axis angel
+* @author: leeqingshui 
+*/
+float Get_Yaxis_Angle(void)
+{
 	angle_y = (float)S_Angle.Angle_DataBuf[1]/32768*180;
+	return angle_y;
+}
+
+/** 
+* @description: Return the Z-axis angel
+* @param  {void} 
+* @return {{float} : the Z-axis angel
+* @author: leeqingshui 
+*/
+float Get_Zaxis_Angle(void)
+{
 	angle_z = (float)S_Angle.Angle_DataBuf[2]/32768*180;
+	return angle_z;
+}
+
+/** 
+* @description: Return the X-axis Angle_Acc
+* @param  {void} 
+* @return {{float} : the X-axis Angle_Acc
+* @author: leeqingshui 
+*/
+float Get_Xaxis_Angle_Acc(void)
+{
+	gyro_x = (float)S_AngleAcc.Angle_Acc_DataBuf[0]/32768*2000;
+	return gyro_x;
+}
+
+/** 
+* @description: Return the Y-axis Angle_Acc
+* @param  {void} 
+* @return {{float} : the Y-axis Angle_Acc
+* @author: leeqingshui 
+*/
+float Get_Yaxis_Angle_Acc(void)
+{
+	gyro_y = (float)S_AngleAcc.Angle_Acc_DataBuf[1]/32768*2000;
+	return gyro_y;
+}
+
+/** 
+* @description: Return the Z-axis Angle_Acc
+* @param  {void} 
+* @return {{float} : the Z-axis Angle_Acc
+* @author: leeqingshui 
+*/
+float Get_Zaxis_Angle_Acc(void)
+{
+	gyro_z = (float)S_AngleAcc.Angle_Acc_DataBuf[2]/32768*2000;
+	return gyro_z;
 }
--- Hardware/USART_Gyroscope/USART_Gyroscope.h
+++ Hardware/USART_Gyroscope/USART_Gyroscope.h
@@ -17,7 +17,6 @@
 
 /* Common macro definitions---------------------------------------------------*/
 
-
 /* Data structure declaration-------------------------------------------------*/
 
 /* 
@@ -45,12 +44,33 @@ typedef struct
 
 /* Function declaration-------------------------------------------------------*/
 
+/* Gyroscope calibration */
+t_FuncRet Gyroscope_Calibration(void);
 /* The parameters of the gyroscope data are analyzed */
 void CopeSerial2Data(unsigned char ucData);
 /* Serial port send command function */
 t_FuncRet Send_Command(uint8_t data[3]);
-/* Data conversion */
-void Get_TrueData(void);
+
+/* Return the X-axis acceleration */
+float Get_Xaxis_Acc(void);
+/* Return the Y-axis acceleration */
+float Get_Yaxis_Acc(void);
+/* Return the Z-axis acceleration */
+float Get_Zaxis_Acc(void);
+
+/* Return the X-axis angel */
+float Get_Xaxis_Angle(void);
+/* Return the Y-axis angel */
+float Get_Yaxis_Angle(void);
+/* Return the Z-axis angel */
+float Get_Zaxis_Angle(void);
+
+/* Return the X-axis Angle_Acc */
+float Get_Xaxis_Angle_Acc(void);
+/* Return the Y-axis Angle_Acc */
+float Get_Yaxis_Angle_Acc(void);
+/* Return the Z-axis Angle_Acc */
+float Get_Zaxis_Angle_Acc(void);
 
 
 #ifdef __cplusplus
